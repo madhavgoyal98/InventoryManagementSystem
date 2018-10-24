0385fa53c77b60df22571fa5794e1253fc845d74
Madhav Goyal <madhavgoyal@live.com>
2018-10-24 17:43:30 +0530

    .

---
+++ input_cleaning.php
@@ -0,0 +1,18 @@
+<?php
+	function sanitizeString($var)
+	{
+		$var = stripslashes($var);
+		$var = htmlentities($var);
+		$var = strip_tags($var);
+
+		return $var;
+	}
+
+	function sanitizeMySQL($connection, $var)
+	{
+		$var = $connection->real_escape_string($var);
+		$var = sanitizeString($var);
+
+		return $var;
+	}
+?>
