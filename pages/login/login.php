609a0aa05a8750549742c5eba320f2736f7a91a8
Madhav Goyal <madhavgoyal@live.com>
2018-10-27 10:01:15 +0530

    removed radio button
    old php code also removed

--- pages/login/login.php
+++ pages/login/login.php
@@ -15,78 +15,6 @@
 </head>
 	
 <?php
-	require("../db_cred.php"); //file for database credentials
-	require("../input_cleaning.php"); //file for string santization functions
-	
-	$connection = new MySQLi($db_host, $db_user, $db_pass, $db_name);
-	$err = "";
-	
-	if($connection->connect_error)
-	{
-		die($connection->connect_error);
-	}
-	
-	if(isset($_POST['submit']))
-	{
-		//santizing input data
-		$email = sanitizeMySQL($connection, $_POST['email']);
-		$password = sanitizeMySQL($connection, $_POST['password']);
-		$type = $_POST['memberType'];
-		
-		$query = "";
-		
-		//check if user type is 'employee' or 'student'
-		if($type == "employee")
-		{
-			$query = "SELECT * FROM employee_login WHERE email='$email';";
-		}
-		else
-		{
-			$query = "SELECT * FROM student_login WHERE email='$email';";
-		}
-		
-		$result = $connection->query($query);
-		
-		if(!$result)
-		{
-			die($connection->connect_error);
-		}
-		elseif($result->num_rows)
-		{
-			$row = $result->fetch_array(MYSQLI_NUM);
-			$result->close();
-			
-			//checking account suspension status
-			if($row[3] == 0)
-			{
-				//storing session data
-				session_start();
-				$_SESSION['sid'] = $row[0];
-				$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
-				$_SESSION['ua'] = $_SERVER['HTTP_USER_AGENT'];
-				
-				//matching the entered password with encoded password saved in database
-				if(password_verify($password, $row[2]))
-				{
-					header('Location: ');  //link to other page
-				}
-				else
-				{
-					$err = "Invalid email/password"; //error message
-				}
-			}
-			else
-			{
-				$err = "Your account has been suspended";
-			}
-		}
-		else
-		{
-			$err = "Invalid email/password";
-		}
-		
-		$connection->close();
-	}
 ?>
 
 <body>
@@ -111,12 +39,6 @@
 			</div>
             <div class="form-group">
 				<input class="form-control" type="password" name="password" placeholder="Password" script="document.getElementById('error').innerHTML='';" required>
-			</div>
-            <div class="form-check">
-				<input class="form-check-input" type="radio" id="formCheck-1" name="memberType" checked="" value="admin"><label class="form-check-label" for="formCheck-1">Admin</label>
-			</div>
-            <div class="form-check">
-				<input class="form-check-input" type="radio" id="formCheck-2" name="memberType" value="worker"><label class="form-check-label" for="formCheck-2">Worker</label>
 			</div>
             <div class="form-group">
 				<button class="btn btn-primary btn-block" type="submit" name="submit">Log In</button>
